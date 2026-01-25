ea15729eb814436ea6687a52289d5fb2dbb3d561
SenhorArroz <luizrob.guimaraes@hotmail.com>
2026-01-24 23:47:19 -0300

    correcao detalhes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -273,44 +273,6 @@ export default async function Home() {
 					</div>
 				</div>
 			</section>
-
-			<footer className="footer footer-center  p-10 bg-base-100 text-base-content/50 border-t border-base-content/5">
-				<div className="grid grid-flow-col gap-4">
-					<a className="link link-hover">Sobre</a>
-					<a className="link link-hover">Contato</a>
-					<a className="link link-hover">Carreiras</a>
-					<a className="link link-hover">Imprensa</a>
-				</div>
-				<div>
-					<div className="grid grid-flow-col gap-4">
-						<a>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								width="24"
-								height="24"
-								viewBox="0 0 24 24"
-								className="fill-current"
-							>
-								<path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 2.468-4.678 3.062-7.57 2.675 4.189 2.688 9.157 1.731 11.209-6.947 1.96-7.859 1.194-11.776-2.106-13.673.953-1.464 1.785-3.107 2.433-4.886z"></path>
-							</svg>
-						</a>
-						<a>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								width="24"
-								height="24"
-								viewBox="0 0 24 24"
-								className="fill-current"
-							>
-								<path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-							</svg>
-						</a>
-					</div>
-				</div>
-				<div>
-					<p>Copyright © 2026 - CashFlow Inc.</p>
-				</div>
-			</footer>
 		</div>
 	);
 }
